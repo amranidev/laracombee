cd79d293ffad67d5f14e809785cf41c6d2472e9c
Amrani Houssain <houssain@airbit.com>
2018-08-13 22:18:29 +0100

    init main class

---
+++ src/Laracombee.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Amranidev\Laracombee;
+
+use Recombee\RecommApi\Client;
+use Recombee\RecommApi\Requests\AddCartAddition;
+use Recombee\RecommApi\Requests\AddDetailView;
+use Recombee\RecommApi\Requests\AddPurchase;
+use Recombee\RecommApi\Requests\AddRating;
+use Recombee\RecommApi\Requests\AddUser;
+use Recombee\RecommApi\Requests\Batch;
+use Recombee\RecommApi\Requests\DeleteCartAddition;
+use Recombee\RecommApi\Requests\DeleteRating;
+use Recombee\RecommApi\Requests\GetUserValues;
+use Recombee\RecommApi\Requests\ListUsers;
+use Recombee\RecommApi\Requests\MergeUsers;
+use Recombee\RecommApi\Requests\RecommendItemsToUser;
+use Recombee\RecommApi\Requests\SetItemValues;
+
+class Laracombee
+{
+    private $client;
+
+    private $timeout;
+
+    public function __constructor() 
+    {
+        $this->client = new Client(config('laracombee.database'), config('laracombee.token'));
+        $this->timeout = config('laracombee.tomeout');
+    }
+
+    public function addItem($item_id, $fields) 
+    {
+        $item = new SetItemValues($item_id, $fields, [
+            "cascadeCreate" => true
+        ]);
+        
+        $item->setTimeout($this->timeout);
+
+        $this->client->send($item);
+        
+        //
+        return true;
+    }
+
+    public function recommendItemsToUser($user_id, $limit, $filters)
+    {
+        $items = new RecommendItemsToUser($user_id, $limit, $filters);
+
+        $items->setTimeout($this->timeout);
+
+        return $this->client->send($items);
+    }
+}
