b8953b86a504073269f9092b880f313b8517bdef
Amrani Houssain <houssain@airbit.com>
2018-08-25 16:44:09 +0100

    Add user properties and item properties

--- publishes/laracombee.php
+++ publishes/laracombee.php
@@ -4,6 +4,14 @@
 
 return [
     'database' => '',
-    'token' => '',
-    'timeout' => 2000
+    'token'    => '',
+    'timeout'  => 2000,
+    'user-properties' => [
+        "name" => "string",
+        "age"  => "boolean",   
+    ],
+    'item-properties' => [
+        "item-name" => "string",
+        "price"     => "int",
+    ]
 ];
