bf4c51e6b5d4841f1a49210036912ccad52d4f1f
Amrani Houssain <houssain@airbit.com>
2018-08-26 18:25:30 +0100

    Update config

--- publishes/laracombee.php
+++ publishes/laracombee.php
@@ -1,15 +1,44 @@
 <?php
 
-// Config
-
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Id and token.
+    |--------------------------------------------------------------------------
+    |
+    | Here where you can define your database ID and recombee token.
+    |
+    */
+
     'database' => '',
     'token'    => '',
+
     'timeout'  => 2000,
+
+    /*
+    |--------------------------------------------------------------------------
+    | User properties.
+    |--------------------------------------------------------------------------
+    |
+    | Here where you can define the item properties in the recombee database.
+    |
+    */
+
     'user-properties' => [
         "name" => "string",
         "age"  => "boolean",   
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Item properties.
+    |--------------------------------------------------------------------------
+    |
+    | Here where you can define the item properties in the recombee database.
+    |
+    */
+
     'item-properties' => [
         "item-name" => "string",
         "price"     => "int",
