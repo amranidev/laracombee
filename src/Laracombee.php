8985982a7de77590c0db610f84a977b107b4b504
Amrani Houssain <houssain@airbit.com>
2018-09-06 22:36:11 +0100

    opt

--- src/Laracombee.php
+++ src/Laracombee.php
@@ -2,10 +2,22 @@
 
 namespace Amranidev\Laracombee;
 
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Model;
+
 class Laracombee extends AbstractRecombee
 {
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function addUserModel(User $user) {
+        // @todo
+    }
+
+    public function addItemModel(Model $model)
+    {
+        // @todo
     }
 }
