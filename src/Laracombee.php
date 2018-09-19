2ba7a65dd4cbc90599346374a109df5e0f9e14a8
Amrani Houssain <houssain@airbit.com>
2018-09-19 21:37:32 +0100

    AddUserModel & AddItemModel functionality

--- src/Laracombee.php
+++ src/Laracombee.php
@@ -7,18 +7,47 @@ use Illuminate\Foundation\Auth\User;
 
 class Laracombee extends AbstractRecombee
 {
+    /**
+     * Create new laracombee instance.
+     */
     public function __construct()
     {
         parent::__construct();
     }
 
+    /**
+     * Add a user to recombee db.
+     *
+     * @param \Illuminate\Foundation\Auth\User $user
+     *
+     * @return \Amranidev\Laracombee\AbstractRecombee
+     */
     public function addUserModel(User $user)
     {
-        // @todo
+        $laracombeeProperties = $user::$laracombee;
+
+        $values = collect($user->toArray())->filter(function ($value, $key) use ($laracombeeProperties) {
+            return isset($laracombeeProperties[$key]);
+        })->all();
+
+        return Laracombee::setUserValues($user->id, $values);
     }
 
-    public function addItemModel(Model $model)
+    /**
+     * Add an item to recombee db.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $item
+     *
+     * @return \Amranidev\Laracombee\AbstractRecombee
+     */
+    public function addItemModel(Model $item)
     {
-        // @todo
+        $laracombeeProperties = $item::$laracombee;
+
+        $values = collect($item->toArray())->filter(function ($value, $key) use ($laracombeeProperties) {
+            return isset($laracombeeProperties[$key]);
+        })->all();
+
+        return Laracombee::setItemValues($item->id, $values);
     }
 }
