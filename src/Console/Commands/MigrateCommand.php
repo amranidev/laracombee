b9c4d1ce9669aa0faabdbda2d1934414eaa4d401
Amrani Houssain <houssain@airbit.com>
2018-09-26 21:02:21 +0100

    rw MigrateCommand

---
+++ src/Console/Commands/MigrateCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Amranidev\Laracombee\Console\Commands;
+
+use Amranidev\Laracombee\Console\LaracombeeCommand;
+use Laracombee;
+
+class MigrateCommand extends LaracombeeCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laracombee:migrate
+    						{type : Catalog type (user or item)}
+    						{--class= : Laravel model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate to recombee';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (!$this->option('class')) {
+            $this->error('--class option is required!');
+            die();
+        }
+
+        $scope = $this->prepareScope()->all();
+
+        Laracombee::batch($scope)
+            ->then(function ($response) {
+                $this->info('Done!');
+            })
+            ->otherwise(function ($error) {
+                $this->error($error);
+            })
+            ->wait();
+    }
+
+    /**
+     * Prepare scope.
+     *
+     * @return mixed
+     */
+    public function prepareScope()
+    {
+        $class = $this->option('class');
+        $properties = $class::$laracombee;
+
+        return collect($properties)->map(function (string $type, string $property) {
+            return $this->{'add' . ucfirst($this->argument('type')) . 'Property'}($property, $type);
+        });
+    }
+}
