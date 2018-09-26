aef1202cc6d1c478303e9c3382eafc6a27acd7e3
Amrani Houssain <houssain@airbit.com>
2018-09-26 21:02:34 +0100

    Update commands in the service provider

--- src/Providers/LaracombeeServiceProvider.php
+++ src/Providers/LaracombeeServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace Amranidev\Laracombee\Providers;
 
-use Amranidev\Laracombee\Commands\AddColumns;
-use Amranidev\Laracombee\Commands\DropColumns;
-use Amranidev\Laracombee\Commands\Migrate;
-use Amranidev\Laracombee\Commands\Rollback;
+use Amranidev\Laracombee\Console\Commands\AddColumnsCommand;
+use Amranidev\Laracombee\Console\Commands\DropColumnsCommand;
+use Amranidev\Laracombee\Console\Commands\MigrateCommand;
+use Amranidev\Laracombee\Console\Commands\RollbackCommand;
+use Amranidev\Laracombee\Console\Commands\SeedCommand;
 use Amranidev\Laracombee\Laracombee;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,18 +25,19 @@ class LaracombeeServiceProvider extends ServiceProvider
 
         $this->commands(
             [
-                Migrate::class,
-                Rollback::class,
-                AddColumns::class,
-                DropColumns::class,
+                MigrateCommand::class,
+                RollbackCommand::class,
+                AddColumnsCommand::class,
+                DropColumnsCommand::class,
+                SeedCommand::class,
             ]
         );
     }
 
     public function boot()
     {
-        $configPath = __DIR__.'/../config/laracombee.php';
+        $configPath = __DIR__ . '/../config/laracombee.php';
         $this->publishes([
-            $configPath => config_path('laracombee.php'), ]);
+            $configPath => config_path('laracombee.php')]);
     }
 }
