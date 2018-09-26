be9239d3eabaf44c5a870409c6bf355bc31deb1a
Amrani Houssain <houssain@airbit.com>
2018-09-26 21:03:27 +0100

    Remove the unnecessary config options

--- config/laracombee.php
+++ config/laracombee.php
@@ -14,39 +14,14 @@ return [
     'database' => '',
     'token'    => '',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Recombee Timeout.
+    |--------------------------------------------------------------------------
+    |
+    | Here where you can define recombee response timeout in milliseconds.
+    |
+    */
+
     'timeout'  => 2000,
-
-    /*
-    |--------------------------------------------------------------------------
-    | User properties.
-    |--------------------------------------------------------------------------
-    |
-    | Here where you can define the item properties in the recombee database.
-    |
-    | Value type of the user property to be created. One of:
-    | int, double, string, boolean, timestamp, set, image or imageList.
-    |
-    */
-
-    'user-properties' => [
-        'name' => 'string',
-        'age'  => 'boolean',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Item properties.
-    |--------------------------------------------------------------------------
-    |
-    | Here where you can define the item properties in the recombee database.
-    |
-    | Value type of the item property to be created. One of:
-    | int, double, string, boolean, timestamp, set, image or imageList.
-    |
-    */
-
-    'item-properties' => [
-        'item-name' => 'string',
-        'price'     => 'int',
-    ],
 ];
