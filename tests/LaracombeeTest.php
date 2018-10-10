71b8332c1556f683a47d67d1a50b41ac65838c15
Amrani Houssain <houssain@airbit.com>
2018-10-10 22:20:37 +0100

    create test for getPropertyItemInfo

--- tests/LaracombeeTest.php
+++ tests/LaracombeeTest.php
@@ -99,13 +99,13 @@ class LaracombeeTest extends TestCase
 
     public function testAddandDeletePurchase()
     {
-        $time = (float) Carbon::now()->timestamp.'.0';
+        $time = (float) Carbon::now()->timestamp . '.0';
         $options = [
-            'timestamp'     => $time,
+            'timestamp' => $time,
             'cascadeCreate' => true,
-            'amount'        => 5,
-            'price'         => 15,
-            'profit'        => 20,
+            'amount' => 5,
+            'price' => 15,
+            'profit' => 20,
         ];
 
         $request = Laracombee::addPurchase($this->userId, $this->itemId, $options);
@@ -137,8 +137,8 @@ class LaracombeeTest extends TestCase
     {
         $options = [
             'cascadeCreate' => true,
-            'amount'        => 5,
-            'price'         => 50,
+            'amount' => 5,
+            'price' => 50,
         ];
 
         $request = Laracombee::addCartAddition($this->userId, $this->itemId, $options);
@@ -196,10 +196,10 @@ class LaracombeeTest extends TestCase
     public function testListItems()
     {
         $options = [
-            'filter'             => '',
-            'count'              => 5,
-            'offset'             => 0,
-            'returnProperties'   => true,
+            'filter' => '',
+            'count' => 5,
+            'offset' => 0,
+            'returnProperties' => true,
             'includedProperties' => ['productName'],
         ];
 
@@ -215,13 +215,26 @@ class LaracombeeTest extends TestCase
         }
     }
 
+    public function testGetItemPropertyInfo()
+    {
+        $request = Laracombee::getItemPropertyInfo('productName');
+
+        $response = Laracombee::send($request)->wait();
+
+        $this->assertInternalType('array', $response);
+        $this->assertArrayHasKey('name', $response);
+        $this->assertArrayHasKey('type', $response);
+        $this->assertEquals('productName', $response['name']);
+        $this->assertEquals('string', $response['type']);
+    }
+
     public function testListUsers()
     {
         $options = [
-            'filter'             => '',
-            'count'              => 5,
-            'offset'             => 0,
-            'returnProperties'   => true,
+            'filter' => '',
+            'count' => 5,
+            'offset' => 0,
+            'returnProperties' => true,
             'includedProperties' => ['firstName'],
         ];
 
