2430bdf6be62010b6addb5eae4172036de0acd1c
Houssain Amrani <amranidev@gmail.com>
2020-01-27 22:50:17 +0000

    Apply fixes from StyleCI

--- src/LaracombeeConnector.php
+++ src/LaracombeeConnector.php
@@ -2,12 +2,11 @@
 
 namespace Amranidev\Laracombee;
 
-use Illuminate\Queue\Connectors\ConnectorInterface;
-
 class LaracombeeConnector
 {
     /**
      * @param array $config
+     *
      * @return Laracombee
      */
     public function connect()
